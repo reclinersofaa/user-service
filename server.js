b4821c606f82251df44ccaaebf637e37e9d6f939
reclinersofaa <dattatreyathe2912@gmail.com>
2025-08-03 01:15:59 +0530

    add prom-client

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
+const promClient = require('prom-client');
+promClient.collectDefaultMetrics();
 
 // Middleware
 app.use(express.json());
